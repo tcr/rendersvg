4bc235d037fcce2cecc5cb437bad45c094f760ed
Tim Cameron Ryan <id@timryan.org>
2014-04-14 16:02:38 -0400

    Updates API to accept buffer or streams

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ Renders SVGs using PhantomJS.
 ```javascript
 var rendersvg = require('rendersvg');
 
-rendersvg(instream, outstream, [ext = 'png'])
+instream.pipe(rendersvg.renderStream([ext = 'png'])).pipe(outstream)
+// or
+rendersvg.convert(inbuffer, [ext = 'png'], function (err, outbuf) {
+	// ...
+})
 ```
 
 Possible extensions:
@@ -17,7 +21,7 @@ Possible extensions:
 
 ## Requirements
 
-```brew update && brew install phantomjs```
+**OS X:** ```brew install phantomjs```
 
 ## License
 
--- index.js
+++ index.js
@@ -4,43 +4,69 @@ var path = require('path');
 var phantom = require('node-phantom');
 var temp = require('temp');
 
-module.exports = function (ins, out, ext) {
-  ext = ext || 'png';
-  var buf = [];
-  ins.on('data', function (data) {
-    buf.push(data);
-  });
-  ins.on('end', function (data) {
-    if (arguments.length) {
-      buf.push(data);
-    }
-    var xml = String(Buffer.concat(buf));
-    phantom.create(function (err, ph) {
-      if (err) { throw err; }
-      ph.createPage(function (err, page) {
-        if (err) { ph.exit(); throw err; }
+function conversion (xml, ext, callback) {
+  phantom.create(function (err, ph) {
+    if (err) { throw err; }
+    ph.createPage(function (err, page) {
+      if (err) { ph.exit(); throw err; }
 
-        page.onLoadFinished = function () {
-          getSvgDimensions(page, function (err, dimensions) {
-            page.set('viewportSize', {
-              width: dimensions.width,
-              height: dimensions.height
-            }, function () {
-              temp.open({suffix: '.' + ext}, function (err, info) {
-                page.render(info.path, function (err) {
-                  fs.createReadStream(info.path).pipe(out);
-                  ph.exit();
-                });
+      page.onLoadFinished = function () {
+        getSvgDimensions(page, function (err, dimensions) {
+          page.set('viewportSize', {
+            width: dimensions.width,
+            height: dimensions.height
+          }, function () {
+            temp.open({suffix: '.' + ext}, function (err, info) {
+              page.render(info.path, function (err) {
+                callback(info.path);
+                ph.exit();
               });
             });
           });
-        };
-
-        page.set('content', xml, function (err) {
-          if (err) { ph.exit(); throw err; }
         });
+      };
+
+      page.set('content', xml, function (err) {
+        if (err) { ph.exit(); throw err; }
       });
     });
+  });
+}
+
+exports.renderStream = function (ext) {
+  ext = ext || 'png';
+
+  var buf = [];
+  var ins = new (require('stream').Duplex)();
+  ins.write = function (chunk, encoding, next) {
+    buf.push(chunk);
+    next();
+  }
+  ins.on('finish', function () {
+    var xml = String(Buffer.concat(buf));
+    conversion(xml, ext, function (imgpath) {
+      var str = fs.createReadStream(imgpath);
+      str.on('data', function (data) {
+        ins.push(data);
+      })
+      str.on('end', function (data) {
+        ins.end(data);
+      })
+    })
+  })
+  return ins;
+}
+
+exports.render = function (buf, ext, next) {
+  if (typeof ext == 'function') {
+    next = ext;
+    ext = 'png';
+  }
+  ext = ext || 'png';
+
+  var xml = buf.toString('utf-8');
+  conversion(xml, ext, function (imgpath) {
+    next(null, fs.readFileSync(imgpath));
   })
 }
 
